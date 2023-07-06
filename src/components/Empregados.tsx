dd9cdb3c3a0e47062c0c66d9c8fa9f457e131596
Diogo <diogo.andre.macieira@gmail.com>
2023-07-06 10:55:12 +0100

    formulário de login

--- src/components/Empregados.tsx
+++ src/components/Empregados.tsx
@@ -6,7 +6,22 @@ const Empregados = () => {
     window.scrollTo(0, 0); // Scroll to the top of the page when the component mounts
   }, []);
   return (
-    <></>
+     <div className="container">
+      <form>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label htmlFor="username">Nº Empregado:</label>
+          <input type="text" id="username" name="username" placeholder="Insira o seu numero de empregado" required />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password:</label>
+          <input type="password" id="password" name="password" placeholder="Insira a sua password" required />
+        </div>
+        <div className="form-group">
+          <input type="submit" value="Login" />
+        </div>
+      </form>
+    </div>
   );
 };
 
